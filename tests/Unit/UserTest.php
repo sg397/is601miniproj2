cc1339203b473466e82d20cee554cef6c89f48bc
sunitha <sg397@njit.edu>
2018-11-02 14:15:45 -0400

    UnitTest: Insert into Users table

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use function Sodium\increment;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserTest extends TestCase
+{
+    /*Insert  User Test*/
+    public function testUserInsert1()
+    {
+        $user = new User;
+
+        $user->id = 99;
+        $user->name = 'Aavaa Bbbzb';
+        $user->email = 'Aaaaa.Bvbbb@gmail.com';
+        $user->password = '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm';
+
+        $user->save();
+        $user = User::findOrFail($user->id);
+
+    }
+
+}
